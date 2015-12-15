a1cc6d90de1d060c7d76be7cb9c3876a621cc3bd
Ole Ersoy <ole.ersoy@gmail.com>
2015-12-15 14:35:26 -0600

    Set org.junit test dependency scope to provided

--- src/main/java/com/fireflysemantics/math/test/TestUtils.java
+++ src/main/java/com/fireflysemantics/math/test/TestUtils.java
@@ -1,4 +1,3 @@
-package com.fireflysemantics.math.test;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,6 +14,7 @@ package com.fireflysemantics.math.test;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.fireflysemantics.math.test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
